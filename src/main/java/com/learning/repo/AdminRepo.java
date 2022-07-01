2bbaea7cb299aabc96897db58cd2c502aeda653b
mardo <mardo@MarLaptop>
2022-07-01 16:56:46 -0400

    Added methods to Admin Controller

--- src/main/java/com/learning/repo/AdminRepo.java
+++ src/main/java/com/learning/repo/AdminRepo.java
@@ -3,7 +3,10 @@ package com.learning.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.learning.entity.Admin;
+import com.learning.entity.Staff;
 
 public interface AdminRepo extends JpaRepository<Admin, Integer>{
+
+	Staff save(Staff staff);
 
 }
--- src/main/java/com/learning/service/AdminService.java
+++ src/main/java/com/learning/service/AdminService.java
@@ -4,12 +4,17 @@ import java.util.List;
 
 import com.learning.entity.Account;
 import com.learning.entity.Admin;
+import com.learning.entity.Staff;
 
 public interface AdminService {
-	public Admin addAdmin(Admin admin);
+	public void addAdmin(Admin admin);
 	public List<Admin> getAllAdmin();
 	public Admin getAdminById(int personId);
 	public Admin updateAdmin(Admin admin);
-	
+	public String deleteAdminById(int personId);
+	public Staff createStaff(Staff staff);
+	public String validateAdmin(String userName, String password);
+	public List<Staff> getAllStaff();
+	public String setStaffStatus();	
 	
 }
